94ef24cdb0971615a54c6f5247246839bcd99d26
nhphat22 <trafaldwaterlaw@gmail.com>
2022-01-26 15:58:17 +0700

    first commit

---
+++ project/server/config.py
@@ -0,0 +1,26 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+postgres_local_base = 'postgresql://postgres:hpnguyen22@localhost:5432/'
+database_name = 'shopping_cart'
+
+
+class BaseConfig:
+    """Base configuration."""
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
+    DEBUG = False
+    BCRYPT_LOG_ROUNDS = 13
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration."""
+    DEBUG = True
+    BCRYPT_LOG_ROUNDS = 4
+    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration."""
+    SECRET_KEY = 'my_precious'
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///example'
